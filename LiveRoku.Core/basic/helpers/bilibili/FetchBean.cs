1d227969d4400ba6fcbfa70963024ffaeffd80e3
px 2 <xiaoxiaodx@outlook.com>
2017-09-15 15:34:10 +0800

    update classifications

---
+++ LiveRoku.Core/basic/helpers/bilibili/FetchBean.cs
@@ -0,0 +1,52 @@
+﻿using LiveRoku.Base;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace LiveRoku.Core {
+    internal class FetchBean {
+        public string OriginRoomIdText { get; private set; }
+        public string RealRoomIdText { get; private set; }
+        public int OriginRoomId { get; private set; }
+        public int RealRoomId { get; private set; }
+        public string FlvAddress { get; private set; }
+        public RoomInfo RoomInfo { get; private set; }
+        public string Folder { get; set; }
+        public string FileFullName { get; set; }
+        public bool AutoStart { get; set; }
+        public bool DanmakuNeed { get; set; }
+        public ILogger Logger { get; set; }
+        private readonly BiliApi biliApi;
+
+        public FetchBean (int originRoomId, BiliApi biliApi) {
+            this.OriginRoomId = originRoomId;
+            this.OriginRoomIdText = originRoomId.ToString ();
+            this.biliApi = biliApi;
+        }
+
+        public Task<bool> refresh () {
+            return Task.Run (() => {
+                var sw = new Stopwatch ();
+                sw.Start ();
+                Logger?.appendLine ("INFO", "-->sw--> start 00m:00s 000");
+                //Try to get real roomId
+                var realRoomIdTextTemp = biliApi.getRealRoomId (OriginRoomIdText);
+                Logger?.appendLine ("INFO", $"-->sw--> fetched real roomId at {sw.Elapsed.ToString("mm'm:'ss's 'fff")}");
+                int realRoomIdTemp;
+                //Try to get flv url
+                if (!string.IsNullOrEmpty (realRoomIdTextTemp) && int.TryParse (realRoomIdTextTemp, out realRoomIdTemp)) {
+                    var flvUrl = biliApi.getRealUrl (realRoomIdTextTemp);
+                    Logger?.appendLine ("INFO", $"-->sw--> fetched real url at {sw.Elapsed.ToString("mm'm:'ss's 'fff")}");
+                    if (!string.IsNullOrEmpty (flvUrl)) {
+                        this.RealRoomId = realRoomIdTemp;
+                        this.RealRoomIdText = realRoomIdTextTemp;
+                        this.FlvAddress = flvUrl;
+                        this.RoomInfo = biliApi.getRoomInfo (RealRoomId);
+                        Logger?.appendLine ("INFO", $"-->sw--> fetched room info at {sw.Elapsed.ToString("mm'm:'ss's 'fff")}");
+                        return true;
+                    }
+                }
+                return false;
+            });
+        }
+    }
+}
--- LiveRoku.Core/downloaders/LiveDownloader.cs
+++ LiveRoku.Core/downloaders/LiveDownloader.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -31,7 +29,7 @@ namespace LiveRoku.Core {
         private readonly IRequestModel model; //Provide base parameters
         private VideoInfo videoInfo;
         private int requestTimeout;
-        private FetchModel settings;
+        private FetchBean settings;
 
         public LiveDownloader (IRequestModel model, string userAgent, int requestTimeout) {
             cancelMgr = new CancellationManager ();
@@ -119,9 +117,9 @@ namespace LiveRoku.Core {
             //Prepare real roomId and flv url
             bool isUpdated = false;
             Task.Run (async () => {
-                settings = new FetchModel (roomId, biliApi);
+                settings = new FetchBean (roomId, biliApi);
                 settings.Logger = this;
-                isUpdated = await settings.updateModel ();
+                isUpdated = await settings.refresh ();
             }, cts.Token).ContinueWith (task => {
                 task.Exception?.printStackTrace ();
                 //Check if get it successful
@@ -188,7 +186,7 @@ namespace LiveRoku.Core {
                     cancelMgr.cancel ("autostart-fetch");
                     cancelMgr.set ("autostart-fetch", cancellation);
                     Task.Run (async () => {
-                        var isUpdated = await settings.updateModel ();
+                        var isUpdated = await settings.refresh ();
                         if (isUpdated && IsRunning && LiveStatus == LiveStatus.Start) {
                             flvFetcher.start (settings.FlvAddress);
                             appendInfoMsg ($"Flv address updated : {settings.FlvAddress}");
@@ -318,51 +316,5 @@ namespace LiveRoku.Core {
 
     }
 
-    internal class FetchModel {
-        public string OriginRoomIdText { get; private set; }
-        public string RealRoomIdText { get; private set; }
-        public int OriginRoomId { get; private set; }
-        public int RealRoomId { get; private set; }
-        public string FlvAddress { get; private set; }
-        public RoomInfo RoomInfo { get; private set; }
-        public string Folder { get; set; }
-        public string FileFullName { get; set; }
-        public bool AutoStart { get; set; }
-        public bool DanmakuNeed { get; set; }
-        private readonly BiliApi biliApi;
-        public ILogger Logger { get; set; }
-
-        public FetchModel (int originRoomId, BiliApi biliApi) {
-            this.OriginRoomId = originRoomId;
-            this.OriginRoomIdText = originRoomId.ToString ();
-            this.biliApi = biliApi;
-        }
-
-        public Task<bool> updateModel () {
-            return Task.Run (() => {
-                var sw = new Stopwatch ();
-                sw.Start ();
-                Logger?.appendLine ("INFO", "-->sw--> start 00m:00s 000");
-                //Try to get real roomId
-                var realRoomIdTextTemp = biliApi.getRealRoomId (OriginRoomIdText);
-                Logger?.appendLine ("INFO", $"-->sw--> fetched real roomId at {sw.Elapsed.ToString("mm'm:'ss's 'fff")}");
-                int realRoomIdTemp;
-                //Try to get flv url
-                if (!string.IsNullOrEmpty (realRoomIdTextTemp) && int.TryParse (realRoomIdTextTemp, out realRoomIdTemp)) {
-                    var flvUrl = biliApi.getRealUrl (realRoomIdTextTemp);
-                    Logger?.appendLine ("INFO", $"-->sw--> fetched real url at {sw.Elapsed.ToString("mm'm:'ss's 'fff")}");
-                    if (!string.IsNullOrEmpty (flvUrl)) {
-                        this.RealRoomId = realRoomIdTemp;
-                        this.RealRoomIdText = realRoomIdTextTemp;
-                        this.FlvAddress = flvUrl;
-                        this.RoomInfo = biliApi.getRoomInfo (RealRoomId);
-                        Logger?.appendLine ("INFO", $"-->sw--> fetched room info at {sw.Elapsed.ToString("mm'm:'ss's 'fff")}");
-                        return true;
-                    }
-                }
-                return false;
-            });
-        }
-    }
 
 }
