92d646874a68bf9951c6fbd5a99097a859f30bde
px 2 <xiaoxiaodx@outlook.com>
2017-10-10 16:39:21 +0800

    fix error

--- LiveRoku.Core/danmaku/DanmakuStorage.cs
+++ LiveRoku.Core/danmaku/DanmakuStorage.cs
@@ -5,12 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using LiveRoku.Base;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading;
 
 namespace LiveRoku.Core {
     public class DanmakuStorage {
         private const string XmlHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><i><chatserver>chat.bilibili.com</chatserver><chatid>0</chatid><mission>0</mission><maxlimit>0</maxlimit><source>k-v</source>";
         private const string XmlFooter = "</i>";
         private readonly ConcurrentQueue<DanmakuModel> danmakuQueue;
+        private CancellationTokenSource cancelWrite;
         private string storagePath;
         private long nowTime;
         private int flushTime;
@@ -55,55 +57,62 @@ namespace LiveRoku.Core {
         }
 
         [SuppressMessage("Microsoft.Performance", "CS4014")]
-        private async void startWrite () {
-            try {
-                writerFs = new FileStream (storagePath, FileMode.Create);
-                writer = new StreamWriter (writerFs, encoding);
-                writer.Write (XmlHeader);
-            } catch (Exception e) {
-                e.printStackTrace ();
+        private void startWrite () {
+            if(cancelWrite != null && cancelWrite.Token.CanBeCanceled) {
+                cancelWrite.Cancel();
             }
-
+            cancelWrite = new CancellationTokenSource();
             Task.Run(async () => {
-                while (IsWriting) {
-                    if (writer == null) break;
-                    lock (locker) {
-                        writer.Flush();
-                    }
-                    await Task.Delay(flushTime);
-                }
-            }).ContinueWith(task => {
-                task.Exception?.printStackTrace();
-            }, TaskContinuationOptions.OnlyOnFaulted);
-            while (IsWriting) {
+                //Write file head part
                 try {
-                    var danmaku = await dequeue ();
-                    if (danmaku == null) continue;
-                    //TODO implements danmakuModel.ToString(datetime) method
-                    lock (locker) {
-                        writer.WriteLine (danmaku.ToString (nowTime));
-                    }
+                    writerFs = new FileStream(storagePath, FileMode.Create);
+                    writer = new StreamWriter(writerFs, encoding);
+                    writer.Write(XmlHeader);
                 } catch (Exception e) {
-                    e.printStackTrace ();
-                    continue;
+                    e.printStackTrace();
                 }
-            }
-            try {
-                writer.Write (XmlFooter);
-                writer.Flush ();
-                writer.Close ();
-                writerFs.Close ();
-            } catch (Exception e) {
-                e.printStackTrace ();
-            }
+                //Flush every flush time
+                Task.Run(async () => {
+                    while (IsWriting) {
+                        if (writer == null) break;
+                        lock (locker) {
+                            writer.Flush();
+                        }
+                        await Task.Delay(flushTime, cancelWrite.Token);
+                    }
+                }, cancelWrite.Token).ContinueWith(task => {
+                    task.Exception?.printStackTrace();
+                }, TaskContinuationOptions.OnlyOnFaulted);
+                //Start writing danmaku
+                while (IsWriting) {
+                    try {
+                        var danmaku = await dequeue();
+                        if (danmaku == null) continue;
+                        //TODO implements danmakuModel.ToString(datetime) method
+                        lock (locker) {
+                            writer.WriteLine(danmaku.ToString(nowTime));
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    writer.Write(XmlFooter);
+                    writer.Flush();
+                    writer.Close();
+                    writerFs.Close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }, cancelWrite.Token);
         }
 
         private async Task<DanmakuModel> dequeue () {
             DanmakuModel result = null;
-            while (danmakuQueue.IsEmpty) {
+            while (IsWriting && danmakuQueue.IsEmpty) {
                 await Task.Delay (100);
             }
-            while (!danmakuQueue.TryDequeue(out result)) { }
+            while (IsWriting && !danmakuQueue.TryDequeue(out result)) { }
             return result;
         }
         
